f9863e47a3c60f7080d608b25186492906b688ff
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-09-23 17:09:57 -0400

    Setup Spring Boot
    This commit adds the Dockerfile and docker compose changes for
    Spring Boot. It will work in the sense that you can go to the port
    that Spring is operating on and see output, but the website is
    not functional yet, only the building is

---
+++ server/src/main/java/simulator/Server/ServerApplication.java
@@ -0,0 +1,21 @@
+package simulator.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ServerApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Deluxe\n"; 
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
