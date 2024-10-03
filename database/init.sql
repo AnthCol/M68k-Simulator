8fac47baab31518ae29553e1a380b3961ce47ea2
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-10-02 21:01:54 -0400

    Setup PostgreSQL + Improve Dockerfiles

---
+++ database/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE files (
+    name VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
+);
+
+
+INSERT INTO files (name, content) VALUES 
+    ('first.x68', 'first'), 
+    ('second.x68', 'second'), 
+    ('third.x68', 'third');
+
