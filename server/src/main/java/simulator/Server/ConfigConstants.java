dcc1f85a6a622e962546eadf32d06d3e32faa5b8
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-10-04 12:54:17 -0400

    Create proper REST controller for Server

--- server/src/main/java/simulator/Server/ConfigConstants.java
+++ server/src/main/java/simulator/Server/ConfigConstants.java
@@ -2,4 +2,5 @@ package simulator.Server;
 
 public class ConfigConstants {
     public static final String clientURL = "http://localhost:5173";
+    public static final String filesPrefixURL = "/api/files";
 }
---
+++ server/src/main/java/simulator/Server/controllers/FileController.java
@@ -0,0 +1,27 @@
+package simulator.Server.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import simulator.Server.ConfigConstants;
+import simulator.Server.datatypes.File;
+
+@CrossOrigin(origins = ConfigConstants.clientURL)
+@RestController
+public class FileController {
+
+    @GetMapping(ConfigConstants.filesPrefixURL + "/get")
+    public List<File> getFiles() {
+        List<File> files = new ArrayList<>();  
+        files.add(new File("First.x68", "First Content"));
+        files.add(new File("Second.x68", "Second Content"));
+        files.add(new File("Third.x68", "Third Content"));
+        return files;
+    }
+}
+
