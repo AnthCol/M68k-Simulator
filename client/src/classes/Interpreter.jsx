bdbba1978d89b49daa51df2b7133d829dd4ff90b
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-10-12 16:45:52 -0400

    Create interpreter backbone

--- client/src/classes/Interpreter.jsx
+++ client/src/classes/Interpreter.jsx
@@ -7,8 +7,30 @@ class Interpreter {
         this.userCode = "";
         this.processor = new Microprocessor();
     }
+
+    tokenize() {
+        // let tokenizer = new Tokenizer();
+        // tokenizer.run();
+    }
+
+    parse() {
+
+    }
+
+    simulate() {
+
+    }
+
+
     // returns final result  
     interpret(code) {
+        // let tokens = tokenize(); 
+        // let validation = parse(tokens);
+        // if (validation != "") {
+        //     return validation;
+        // }
+        // let simulationOutput = simulate(tokens);        
+
         this.userCode = code;        
         return this.userCode;
     }
