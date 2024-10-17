0cc11266433d60fe188ad8de3e154df7263d01f2
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-10-17 15:32:53 -0400

    Add Token class for Tokenizer

---
+++ client/src/classes/Token.jsx
@@ -0,0 +1,22 @@
+class Token { 
+
+    constructor(lexeme, lineNumber, tokenType){
+        this.lexeme = lexeme;
+        this.lineNumber = lineNumber;
+        this.tokenType = tokenType;
+    }
+
+    getLexeme() {
+        return this.lexeme;
+    }
+
+    getLineNumber() {
+        return this.lineNumber;
+    }
+
+    getTokenType() {
+        return this.tokenType;
+    }
+}
+
+export default Token;
